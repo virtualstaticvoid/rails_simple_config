c0415d699d9b4377c39a85e6528cee2303987bce
Ife Ologbese <iologbese@confluence.com>
2021-11-17 11:04:03 +0200

    Rails 6 compatibility

--- lib/rails_simple_config/railtie.rb
+++ lib/rails_simple_config/railtie.rb
@@ -12,7 +12,7 @@ module RailsSimpleConfig
     if Rails.env.development?
       initializer :load_simple_config_development do
         ActionController::Base.class_eval do
-          if Rails::VERSION::MAJOR >= 5 && Rails::VERSION::MINOR >= 1
+          if Rails::VERSION::MAJOR >= 6 || (Rails::VERSION::MAJOR >= 5 && Rails::VERSION::MINOR >= 1)
             prepend_before_action { ::SimpleConfig.reload! }
           else
             prepend_before_filter { ::SimpleConfig.reload! }
